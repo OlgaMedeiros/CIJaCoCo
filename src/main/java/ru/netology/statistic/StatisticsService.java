475d042e200f9702bc51eb32ebf7bafac0bf3a49
OlgaMed <mama2482@mail.ru>
2023-03-05 20:42:09 +0400

    finish

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -3,14 +3,15 @@
 package ru.netology.statistic;
 
 public class StatisticsService {
-    public long findMax(long[] incomes) {
+    public long findMax(long[] incomes) {              //рассчитать индекс макксимального дохода
         long current_max_index = 0;
         long current_max = incomes[0];
 
         for (long income : incomes)
-            if (current_max < income)
+            if (current_max < income)                 //массив дохода - incomes
                 current_max = income;
 
-        return current_max;
+        return current_max;                          //первое макксимальное значение
+
+        }
     }
-}
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -13,7 +13,6 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
@@ -23,11 +22,23 @@ public class StatisticsServiceTest {
     void findMax2() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInEuro = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInMillions = {5, 8, 12, 4, 5, 3, 8, 6, 11, 11, 12};
         long predicted = 12;
 
-        long actual = service.findMax(incomesInEuro);
+        long actual = service.findMax(incomesInMillions);
 
         assertEquals(predicted, actual);
     }
+
+    @Test
+    void findMax3() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInDollar = {5, 8, 12, 13, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 13;
+        long actual = service.findMax(incomesInDollar);
+        assertEquals(expected, actual);
+
+    }
+
 }
